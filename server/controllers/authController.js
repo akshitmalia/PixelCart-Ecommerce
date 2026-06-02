2673813c0e0af54df97dc522a14a9364683f24a2
akshitmalia <akshitmalia2005@gmail.com>
2026-06-03 02:22:01 +0530

    authController.js secure and samSite parms changed

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -114,7 +114,7 @@ async function verifyOTP(req, res) {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
+      secure: true,
       sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
