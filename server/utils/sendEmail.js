39edad9dc91007bd289a5d005624d6e8f010d418
akshitmalia <akshitmalia2005@gmail.com>
2026-06-03 02:33:27 +0530

    fix SMTP port 587 for Render deployment

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -3,7 +3,9 @@ import nodemailer from "nodemailer";
 async function sendEmail(to, otp) {
   try {
     const transporter = nodemailer.createTransport({
-      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
