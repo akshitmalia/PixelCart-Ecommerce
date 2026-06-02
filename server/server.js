6838202cab9040b0fbec8edb432bf361224f72f1
akshitmalia <akshitmalia2005@gmail.com>
2026-06-03 01:26:53 +0530

    cors inside the server.js updated

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(cors({
   origin: [
     "http://localhost:3000",
-    "https://pixelcart.vercel.app",  //Will be updated agian
+    "https://pixel-cart-ecommerce.vercel.app",  //Will be updated agian
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
